dc397843f224bb8a3093eae7d8480c942056707a
Lawrence Hook <lawrencehook@gmail.com>
2024-12-22 01:24:01 -0500

    Finish the deploy script

--- preprocessing/scripts/5_deploy.sh
+++ preprocessing/scripts/5_deploy.sh
@@ -4,3 +4,5 @@ set -e
 # Production
 scp data/final.json lightsail:/home/bitnami/github/SqueexVodSearch/app/data/squeex.json
 scp data/full.json  lightsail:/home/bitnami/github/SqueexVodSearch/app/data/squeex_full.json
+
+ssh lightsail "bash /home/bitnami/github/deployments/run"
